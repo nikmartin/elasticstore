994e13672451e511b862810d7ad82442d254e865
Nik Martin <nik.martin@gmail.com>
2020-02-27 15:44:57 -0600

    add references

--- src/references.ts
+++ src/references.ts
@@ -3,10 +3,19 @@ import { Reference } from "./types";
 // Records should be added here to be indexed / made searchable
 const references: Array<Reference> = [
   {
-    collection: 'users',
-    type: 'users',
-    index: 'users',
-    include: ['firstName', 'lastName', 'email'],
+    collection: 'emails',
+    type: 'emails',
+    index: 'emails',
+    include: ['snippet', 'from', 'id'],
+    transform: (data, parent) => ({
+      ...data, from: () => {
+        data.payload.headers.map((h: any) => {
+          if (h.name === 'From') {
+            return h.value;
+          }
+        })
+      }
+    })
   }
 ]
 
